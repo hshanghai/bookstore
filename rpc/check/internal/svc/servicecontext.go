89a72fc30d611dcb27d4aea68e70b8b6fef7aa56
韩上海 <han.shanghai@scimall.org.cn>
2020-12-21 23:21:06 +0800

    初始化

---
+++ rpc/check/internal/svc/servicecontext.go
@@ -0,0 +1,19 @@
+package svc
+
+import (
+	"bookstore/rpc/check/internal/config"
+	"bookstore/rpc/model"
+	"github.com/tal-tech/go-zero/core/stores/sqlx"
+)
+
+type ServiceContext struct {
+	c config.Config
+	Model model.BookModel   // 手动代码
+}
+
+func NewServiceContext(c config.Config) *ServiceContext {
+	return &ServiceContext{
+		c: c,
+		Model: model.NewBookModel(sqlx.NewMysql(c.DataSource), c.Cache), // 手动代码
+	}
+}
